a8adcf76d06c1eb97c6c660d14e29d8b7c6bdcbe
destiny <thiongokelvin5@gmail.com>
2019-02-13 19:50:18 +0300

    Relationships

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
 }
