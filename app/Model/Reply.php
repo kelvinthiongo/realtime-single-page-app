130a8a43c61fc7d09e52d6932b372c3a5d2b827c
Kelvin Maina <thiongokelvin5@gmail.com>
2019-02-23 11:39:26 +0300

    CRUD for Reply

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -2,11 +2,16 @@
 
 namespace App\Model;
 
+use App\User;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    protected $fillable = [
+        'body', 'user_id'
+    ];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
